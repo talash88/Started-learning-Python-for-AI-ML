c72e1fa6504fd3602c593ed9b3ddea471ab47196
Tejas Khadse <tejaskhadse900@gmail.com>
2026-07-05 01:31:18 +0530

    Started learning Python for AI/ML test

---
+++ day02/new2.py
@@ -0,0 +1,14 @@
+
+#Write  Python programs to print the contents of a directory using the os module. Search online for the function which does that
+
+
+
+import os
+
+path = "."
+
+for root, dirs, files in os.walk(path):
+    print("Folder:", root)
+
+    for file in files:
+        print("  File:", file)
